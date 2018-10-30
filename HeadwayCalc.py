f87acc08a2524a044e4202048212b68c6ca24f45
MileCraig <44293228+MileCraig@users.noreply.github.com>
2018-10-30 12:50:45 +0000

    Update HeadwayCalc.py

--- HeadwayCalc.py
+++ HeadwayCalc.py
@@ -1,9 +1,11 @@
 #Headway Calc
 #test
 
+signum=input("What is the num ber of signals required?")
 siglist=[]
 signumend=0
 while float(signumend) < float(signum):
       signumend=signumend+1
       sig=input("enter signal number "+str(signumend)+" ... ")
       siglist=siglist+[str(sig)]
+print(siglist)
