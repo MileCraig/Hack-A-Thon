68ff1c5141b5499241b149c6c08ad3568fc26c4b
JTAther <44293699+JTAther@users.noreply.github.com>
2018-10-30 15:41:01 +0000

    Update Vector Creation.py

--- Vector Creation.py
+++ Vector Creation.py
@@ -8,12 +8,12 @@ Exploring the generation of a vector for use in
 
 length = int(input("How long do you want your vector? \n"))
 
-# Creating a vector of l rows by m columns
+# Creating a row of 'l' columns
 a = ([0] * length)         # Creates a row of 0s 'l' characters long
 
 # Creates an array 'l' long with with entries 1 to 'l'
-for i in range(length):    # Creates for loop between 1 and l
-    a[i]=i+1               # States the 'i'th entry of a is i
+for i in range(length):    # Creates for loop between 1 and 'l'
+    a[i]=i+1               # States the 'i'th entry of a is 'i'
     
 print(a)
 
